a3bf5228b9e65e3eb2b9ccea159201620e224e1e
Enric Tobella <enric.tobella@dixmit.com>
2026-01-24 10:41:35 +0100

    [ADD] vcp_github

---
+++ vcp_github/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2026 Dixmit
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Vcp Github",
+    "summary": """Integrate Virtual Control Platform with Github""",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Dixmit,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/version-control-platform",
+    "depends": [
+        "vcp",
+    ],
+    "external_dependencies": {
+        "python": ["github3.py"],
+    },
+    "data": [],
+    "demo": [],
+}
---
+++ vcp_github/models/__init__.py
@@ -0,0 +1,3 @@
+from . import vcp_platform
+from . import vcp_repository
+from . import res_partner
