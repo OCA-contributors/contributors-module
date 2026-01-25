7dbbda57ac65613634f8b9c10e73b3f18bf42087
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-01-25 01:25:14 +0100

    [REF] vcp: split into vcp and website_partner to make the module more modular. Rational: vcp contains a lot of thing that are interesting to use, even if website is not installed [ADD] website_partner description and screenshot

---
+++ vcp_website_partner/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 GRAP
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Vcp - Website Partner",
+    "summary": "Glue module between Virtual Control Platform and Website Partner",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Dixmit,GRAP,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/version-control-platform",
+    "depends": ["vcp", "website_partner"],
+    "data": [
+        "templates/templates.xml",
+    ],
+    "demo": [],
+    "auto_install": True,
+}
